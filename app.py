b4ba6697fb25159f1d3a4b16b4bdd45ddde27219
dangrous <Daniel@dangercomp.attlocal.net>
2016-07-26 12:45:41 -0700

    Switched to "Player" and indent w/spaces
    and added votes into DB

--- app.py
+++ app.py
@@ -22,21 +22,21 @@ def home():
 @app.route('/data', methods=['POST'])
 def get_game_data():
     room = Room.get(Room.id == request.form['room'])
-    users = User.select().where(User.room_id == room.id)
+    players = Player.select().where(Player.room_id == room.id)
     submission_count = Submission.select().where(Submission.room == room).count()
     if submission_count > 0:
-    	if submission_count == room.num_players:
-    		submissions = Submission.select().where(Submission.room == room).order_by(Submission.randomizer)
-    	else:
-    		submissions = Submission.select().where(Submission.room == room)
-    	submissions = [model_to_dict(submission) for submission in submissions]
+        if submission_count == room.num_players:
+            submissions = Submission.select().where(Submission.room == room).order_by(Submission.randomizer)
+        else:
+            submissions = Submission.select().where(Submission.room == room)
+        submissions = [model_to_dict(submission) for submission in submissions]
     else:
-    	submissions = ''
-    return jsonify(gamedata=model_to_dict(room), submissions=submissions, players=[model_to_dict(user) for user in users])
+        submissions = ''
+    return jsonify(gamedata=model_to_dict(room), submissions=submissions, players=[model_to_dict(player) for player in players])
 
 @app.route('/connect', methods=['POST'])
 def join_game():
-    user = User.create(
+    player = Player.create(
         name=request.form['name']
     )
     if request.form['action'] == "start":
@@ -44,7 +44,7 @@ def join_game():
         code = get_room_code()
         room = Room.create(
             code=code,
-            owner=user
+            owner=player
         )
         owner = True
     else:
@@ -53,17 +53,17 @@ def join_game():
     # When many people joining quickly, this will not work I think.
     room.num_players += 1
     room.save()
-    user.room_id = room.id
+    player.room_id = room.id
     # Because this won't match
-    user.order_in_room = room.num_players
-    user.save()
-    users = User.select().where(User.room_id == room.id)
-    return jsonify(players=[model_to_dict(user) for user in users], gamedata=model_to_dict(room), self=model_to_dict(user))
+    player.order_in_room = room.num_players
+    player.save()
+    players = Player.select().where(Player.room_id == room.id)
+    return jsonify(players=[model_to_dict(player) for player in players], gamedata=model_to_dict(room), self=model_to_dict(player))
 
 @app.route('/start', methods=['POST'])
 def start_game():
     room = Room.get(Room.id == request.form['room'])
-    first_player = User.get(User.room_id == room.id, User.order_in_room == random.randint(1, room.num_players))
+    first_player = Player.get(Player.room_id == room.id, Player.order_in_room == random.randint(1, room.num_players))
     room.leader = first_player
     room.num_round = 1
     room.save()
@@ -82,75 +82,74 @@ def handle_prompts():
 
 @app.route('/submit', methods=['POST'])
 def handle_submissions():
-	room = Room.get(Room.id == request.form['room'])
-	user = User.get(User.id == request.form['self'])
-	submission = Submission.create(
-		text = request.form['submission'],
-	    author = user,
-	    room = room,
-	    randomizer = random.random()
-	)
-	return "Submission added"
+    room = Room.get(Room.id == request.form['room'])
+    player = Player.get(Player.id == request.form['self'])
+    submission = Submission.create(
+        text = request.form['submission'],
+        author = player,
+        room = room,
+        randomizer = random.random()
+    )
+    return "Submission added"
 
 @app.route('/reveal', methods=['POST'])
 def reveal_submission():
-	submission = Submission.get(Submission.id == request.form['submission'])
-	submission.show_auth = True
-	submission.save()
-	return "Submission revealed."
+    submission = Submission.get(Submission.id == request.form['submission'])
+    submission.show_auth = True
+    submission.save()
+    return "Submission revealed."
 
 @app.route('/advance', methods=['POST'])
 def advance_round():
-	delete_subs = Submission.delete().where(Submission.room_id == request.form['room'])
-	delete_subs.execute()
-	room = Room.get(Room.id == request.form['room'])
-	room.num_round = room.num_round + 1
-	next_player = User.get(User.room_id == room.id, User.order_in_room == room.leader.order_in_room % room.num_players + 1)
-	room.leader = next_player
-	room.prompt = None
-	room.votes = 0
-	room.save()
-	users = User.select().where(User.room_id == room.id)
-	for user in users:
-		user.has_voted = False
-		user.save()
-	return "Begun next round."
+    delete_subs = Submission.delete().where(Submission.room_id == request.form['room'])
+    delete_subs.execute()
+    room = Room.get(Room.id == request.form['room'])
+    room.num_round = room.num_round + 1
+    next_player = Player.get(Player.room_id == room.id, Player.order_in_room == room.leader.order_in_room % room.num_players + 1)
+    room.leader = next_player
+    room.prompt = None
+    room.votes = 0
+    room.save()
+    players = Player.select().where(Player.room_id == room.id)
+    for player in playerss:
+        player.has_voted = False
+        player.save()
+    return "Begun next round."
 
 @app.route('/vote', methods=['POST'])
 def vote():
-	submission = Submission.get(Submission.id == request.form['submission'])
-	submission.votes += 1
-	submission.save()
-	room = submission.room
-	room.votes += 1
-	room.save()
-	author = submission.author
-	user = User.get(User.id == request.form['player'])
-	user.has_voted = True
-	if not author == room.leader:
-		author.score += 1
-	else:
-		user.score += 1
-	author.save()
-	user.save()
-	all_subs = Submission.select().where(Submission.room == room)
-	vote_count = 0
-	for submission in all_subs:
-		vote_count += submission.votes
-	if vote_count == room.num_players:
-		for submission in all_subs:
-			submission.show_auth = True
-			submission.save()
-	return "vote counted"
+    submission = Submission.get(Submission.id == request.form['submission'])
+    submission.votes += 1
+    submission.save()
+    room = submission.room
+    room.votes += 1
+    room.save()
+    author = submission.author
+    player = Player.get(Player.id == request.form['player'])
+    player.has_voted = True
+    if not author == room.leader:
+        author.score += 1
+    else:
+        player.score += 1
+    author.save()
+    player.save()
+    vote = Vote.create(
+        room = room,
+        voter = player,
+        submission = submission
+    )
+    all_subs = Submission.select().where(Submission.room == room)
+    vote_count = 0
+    for submission in all_subs:
+        vote_count += submission.votes
+    if vote_count == room.num_players:
+        for submission in all_subs:
+            submission.show_auth = True
+            submission.save()
+    return "vote counted"
 
 def get_room_code():
     return ''.join(random.choice(string.ascii_uppercase) for n in range(5))
-
-# need something to reveal individual submissions
-
-# need something to randomize and reveal text of all submissions once round ready
-
-# need something to change roles in round (I think just round leader)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -7,7 +7,7 @@ class BaseModel(Model):
     class Meta:
         database = db
 
-class User(BaseModel):
+class Player(BaseModel):
     id = PrimaryKeyField()
     name = CharField()
     room_id = IntegerField(null=True)
@@ -18,8 +18,8 @@ class User(BaseModel):
 class Room(BaseModel):
     id = PrimaryKeyField()
     code = CharField(unique=True)
-    owner = ForeignKeyField(User, related_name='owner_of')
-    leader = ForeignKeyField(User, related_name='leader_of', null=True)
+    owner = ForeignKeyField(Player, related_name='owner_of')
+    leader = ForeignKeyField(Player, related_name='leader_of', null=True)
     num_players = IntegerField(default=0)
     last_active = DateTimeField(default=datetime.datetime.now)
     # do I need this next one?
@@ -31,12 +31,18 @@ class Room(BaseModel):
 class Submission(BaseModel):
     id = PrimaryKeyField()
     text = CharField()
-    author = ForeignKeyField(User)
+    author = ForeignKeyField(Player)
     room = ForeignKeyField(Room)
     show_auth = BooleanField(default=False)
     randomizer = FloatField()
     votes = IntegerField(default=0)
 
+class Vote(BaseModel):
+    id = PrimaryKeyField()
+    room = ForeignKeyField(Room)
+    voter = ForeignKeyField(Player, related_name='voted_for')
+    submission = ForeignKeyField(Submission, related_name='vote_s')
+
 def initialize_db():
     db.connect()
-    db.create_tables([Submission, User, Room], safe=True)
+    db.create_tables([Submission, Player, Room], safe=True)
